236482ce4d56564d20a4b7b1f1d2a6bdf8b619d5
root <verdurin@fedoraproject.org>
2013-05-14 10:24:02 +0100

    Fix glexec pool account creation script

--- boxgrinder/create-glexec-users.sh
+++ boxgrinder/create-glexec-users.sh
@@ -2,5 +2,5 @@
 # Create the pool users needed for glexec
 groupadd -g 30000 lt2-cms
 for user in $(seq 0 1 9);
-	do useradd -b /home/grid -u 30000$user -g 30000 -m;
+	do useradd -b /home/grid -u 30000$user -g 30000 -m glx-cms-90$user; 
 done;
