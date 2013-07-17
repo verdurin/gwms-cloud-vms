477c6416c982bf87768ef0f99beae4ec6a8f96a0
root <verdurin@fedoraproject.org>
2013-07-17 11:04:38 +0100

    First attempt at glexec configuration with local pool accounts

--- boxgrinder/create-glexec-users.sh
+++ boxgrinder/create-glexec-users.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Create the pool users needed for glexec
-groupadd -g 30000 lt2-cms
+groupadd -g 30000 lt2-glexec
 for user in $(seq 30000 1 30400);
-	do useradd -b /home/grid -u $user -g 30000 -m glx-cms-$user; 
+	do useradd -b /home/grid -u $user -g 30000 -m lt2-glexec-$user; 
 done
